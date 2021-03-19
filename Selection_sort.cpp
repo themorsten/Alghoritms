913bdb33ca79097c56c50164e2c7749f34a8d213
themorsten <75784716+themorsten@users.noreply.github.com>
2021-03-19 15:40:00 +0300

    Delete Selection_sort.cpp

--- Selection_sort.cpp
+++
@@ -1,9 +0,0 @@
-void Selection_sort(int* A, const int size) {
-	for (int i = 0; i < size-1; i++) {
-		int min = i;
-		for (int j=i+1; j < size - 1; j++) {
-			if (A[min] > A[j]) min = j;
-		}
-		swap(A[min], A[i]);
-	}
-}
